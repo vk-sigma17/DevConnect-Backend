d63b46b13ea159dfa87b1e4f8fabbdc573bd61f3
vikash <khowalvikash@gmail.com>
2025-01-21 21:04:49 +0530

    exploring Get, Post & Put Request

--- src/app.js
+++ src/app.js
@@ -2,21 +2,33 @@ const express = require('express');
 const app = express( )
 
 
+// it will give same response on any req (get, put, post) bcuz order matter
+// app.use("/user",  (req, res) => {
+//     res.send("Hahaha");
+// })
 
 
+// This will only handle GET call to /user
+app.get("/user",  (req, res) => {
+    res.send({firstName:  "Vikash", lastName: "Khowal"});
+})
+ 
+app.post('/user', (req, res) => {
+    // saving Data to Db
+    res.send("Data Successfully saved to database"); 
+})
+app.delete('/user', (req, res) => {
+    // saving Data to Db
+    res.send("Dalated Successfully!!"); 
+})
+
+
+//this will match all the HTTP method API calls to /test
 app.use('/test', (req, res) => {
     console.log('Request to /test');
-    res.send("Hello from test");
 });
 
-app.use('/hello', (req, res) => {
-    console.log('Request to /hello');
-    res.send("Hello Hello");
-});
 
-app.use('/', (req, res) => {
-    res.send("Hello vikash!!")
-})
 
 // app.use('/test', (req, res) => { // fn is called req handler
     // res.send("Hello from test");
